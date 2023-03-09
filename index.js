ecc2e1438aab4ab6789b3223ae7b144bdc92bf8e
RAJAVALLI-K <rajavalli1932bca@gmail.com>
2023-03-09 16:58:26 +0530

    add js-check the large number

---
+++ index.js
@@ -0,0 +1,13 @@
+var a = -30, b = 0, c = 70, d = -50;
+if (a > b && a > c && a > d) {
+    console.log('the number is a  large number:-30');
+}
+else if (b > a && b > c && b > d) {
+    console.log('the number is b  large number:0');
+}
+else if (c > a && c > b && c > d) {
+    console.log('the number is c  large number:70');
+}
+else {
+    console.log('the number is d  large number:-50');
+}
